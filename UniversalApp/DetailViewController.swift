fcba0ec37b740fd64e425c5f684d452377f9b4dd
ZHIYUAN CHEN <kate@kates-imac.local>
2019-04-23 14:04:34 +1000

    Add import CoreLocation line Add IBOutlet variables Modified configureView() to displayData() Change the default variable and function names

--- UniversalApp/DetailViewController.swift
+++ UniversalApp/DetailViewController.swift
@@ -5,36 +5,47 @@
 //  Created by Kate Chen on 9/4/19.
 //  Copyright © 2019 Kate Chen. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
 
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
+    @IBOutlet weak var placeNameLabel: UILabel!
+    @IBOutlet weak var placeAddressLabel: UILabel!
+    @IBOutlet weak var placeLatitudeLabel: UILabel!
+    @IBOutlet weak var placeLongitudeLabel: UILabel!
+    
+    
+    
+    /// display the data to the label
+    func displayData() {
+        // initialise
+        guard let detail = placeDetail else { return }
+        // display data
+        if let nameLabel = placeNameLabel,
+           let addressLabel = placeAddressLabel,
+           let latitudeLabel = placeLatitudeLabel,
+           let longitudeLabel = placeLongitudeLabel {
+            nameLabel.text = detail.placeName
+            addressLabel.text = detail.placeAddress
+            latitudeLabel.text = "\(detail.placeLatitude)"
+            longitudeLabel.text = "\(detail.placeLongitude)"
+        } else { return }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
+        displayData()
     }
 
-    var detailItem: NSDate? {
+    var placeDetail: Place? {
         didSet {
             // Update the view.
-            configureView()
+            displayData()
         }
     }
-
-
+ 
+    
 }
-
