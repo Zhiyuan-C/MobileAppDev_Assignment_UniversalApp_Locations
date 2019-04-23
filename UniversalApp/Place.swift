0daee1797f9e4e059d07e428a8204ae959e58bf7
ZHIYUAN CHEN <kate@kates-imac.local>
2019-04-23 14:07:33 +1000

    Add Place Class file

---
+++ UniversalApp/Place.swift
@@ -0,0 +1,23 @@
+//
+//  Place.swift
+//  UniversalApp
+//
+//  Created by Kate Chen on 9/4/19.
+//  Copyright © 2019 Kate Chen. All rights reserved.
+//
+
+import Foundation
+
+/// Place class, which contains place name, address, latitude and longitude
+class Place {
+    var placeName: String
+    var placeAddress: String
+    var placeLatitude: Double
+    var placeLongitude: Double
+    init(placeName: String, placeAddress: String, placeLatitude: Double, placeLongitude: Double) {
+        self.placeName = placeName
+        self.placeAddress = placeAddress
+        self.placeLatitude = placeLatitude
+        self.placeLongitude = placeLongitude
+    }
+}
