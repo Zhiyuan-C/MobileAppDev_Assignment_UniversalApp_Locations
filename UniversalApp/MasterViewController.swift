f5b3e2a8096c4424db70d6beca7aedee1fd3403f
ZHIYUAN CHEN <kate@kates-imac.local>
2019-04-23 14:06:15 +1000

    Modified MasterView to meet assignment2 milestone 1 requirement

--- UniversalApp/MasterViewController.swift
+++ UniversalApp/MasterViewController.swift
@@ -5,22 +5,29 @@
 //  Created by Kate Chen on 9/4/19.
 //  Copyright © 2019 Kate Chen. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    // initialise varibles
+    /// DetailViewController
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
-
+    /// places which contains array of Place
+    var places = [Place]()
+    /// flag to check if is edit mode
+    var editPlaceFlag = false
+    /// int for determine which row is selected to edit
+    var selectedIndexRowForEdit: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         navigationItem.leftBarButtonItem = editButtonItem
-
+        // add item
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         navigationItem.rightBarButtonItem = addButton
+        
+        // split view
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -31,60 +38,138 @@ class MasterViewController: UITableViewController {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
+    // MARK: - Segues
+    
     @objc
+    /// perform segue way to display AddPlaceViewController
+    ///
+    /// - Parameter sender: self, MasterViewController
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+        performSegue(withIdentifier: "displayAddPlaceView", sender: self)
     }
 
-    // MARK: - Segues
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // if identifier is showDetail, delegate to the detailViewController
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = places[indexPath.row]
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.placeDetail = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+        // if identifier is the displayAddPlaceView, delegate to the AddPlaceViewController
+        else if segue.identifier == "displayAddPlaceView" {
+            guard let addPlaceVC = (segue.destination as! UINavigationController).topViewController as? AddPlaceViewController else { return }
+            addPlaceVC.addPlaceDelegate = self
+            if editPlaceFlag {
+                addPlaceVC.edit = true
+                addPlaceVC.placeData = places[selectedIndexRowForEdit]
             }
         }
     }
 
     // MARK: - Table View
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
+    
+    // how many rows to display
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return places.count
     }
-
+    // display data to the cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = places[indexPath.row]
+        cell.textLabel?.text = object.placeName
         return cell
     }
-
+    // allow to edit item
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    // Rearranging the item.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to toIndexPath: IndexPath) {
+        let place = places.remove(at: fromIndexPath.row)
+        places.insert(place, at: toIndexPath.row)
+    }
+    
+    // MARK: - Swipe actions
+    
+    // Perfomr swipe actions - edit and delete
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let delete = deleteAction(at: indexPath)
+        let edit = editAction(at: indexPath)
+        
+        return UISwipeActionsConfiguration(actions: [delete, edit])
+    }
+    
+    /// Create delete action, to delete the item from table
+    ///
+    /// - Parameter indexPath: indexPath of the selected row
+    /// - Returns: UIContextualAction
+    func deleteAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
+            self.places.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            completion(true)
+        }
+        return action
+    }
+    
+    /// Create edit action, to edit the item of selected row
+    ///
+    /// - Parameter indexPath: indexPath of the selected row
+    /// - Returns: UIContextualAction
+    func editAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .normal, title: "Edit") { (action, view, completion) in
+            self.editPlaceFlag = true
+            self.selectedIndexRowForEdit = indexPath.row
+            self.performSegue(withIdentifier: "displayAddPlaceView", sender: self)
+            completion(true)
+        }
+        action.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        return action
+    }
+    
+    /// Let view pop back to the MasterViewController
+    func backToMaster(){
+        guard let navigationViewControllers = self.navigationController?.viewControllers else { return }
+        for controller in navigationViewControllers {
+            if controller.isKind(of: MasterViewController.self) {
+                self.navigationController?.popToViewController(controller, animated: true)
+                break
+            }
         }
     }
 
+}
 
+//MARK: - AddPlaceVC Delegate
+extension MasterViewController: AddPlaceVCDelegate {
+    
+    /// reload table view
+    func reloadTableView(){
+        tableView.reloadData()
+    }
+    
+    /// append place to places
+    ///
+    /// - Parameter newPlace: new Place created in AddPlaceViewController
+    func addPlace(newPlace: Place){
+        places.append(newPlace)
+        backToMaster()
+        reloadTableView()
+    }
+    
+    /// Pop view back to master, and reload the table view
+    func editPlace() {
+        backToMaster()
+        reloadTableView()
+    }
+    
 }
 
