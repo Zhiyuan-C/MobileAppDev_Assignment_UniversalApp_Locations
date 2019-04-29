cb13d2297677b79ba9daae342c973077bee808fe
ZHIYUAN CHEN <kate@kates-imac.local>
2019-04-29 23:43:54 +1000

    change the order of backToMaster func into extension section

--- UniversalApp/MasterViewController.swift
+++ UniversalApp/MasterViewController.swift
@@ -135,6 +135,15 @@ class MasterViewController: UITableViewController {
         action.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
         return action
     }
+}
+
+//MARK: - AddPlaceVC Delegate
+extension MasterViewController: AddPlaceVCDelegate {
+    
+    /// reload table view
+    func reloadTableView(){
+        tableView.reloadData()
+    }
     
     /// Let view pop back to the MasterViewController
     func backToMaster(){
@@ -145,16 +154,6 @@ class MasterViewController: UITableViewController {
                 break
             }
         }
-    }
-
-}
-
-//MARK: - AddPlaceVC Delegate
-extension MasterViewController: AddPlaceVCDelegate {
-    
-    /// reload table view
-    func reloadTableView(){
-        tableView.reloadData()
     }
     
     /// append place to places
